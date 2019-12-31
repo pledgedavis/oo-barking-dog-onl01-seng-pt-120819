730d0b79054a8a9269e3ebbeff8fecaf76841a58
pledgedavis <pledgedavis14@gmail.com>
2019-12-31 00:48:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+
+class Dog
+  
+  fido = Dog.new
+  # fido = Dog.new
+  
+  # fido.name = "Fido"
+  
+  
+end
+
+
+# def  fido(name)
+
+# fido = Dog.new
+# fido.name = "Fido"
+ 
+
+
+
+
+
+
+
